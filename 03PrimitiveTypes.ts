53f4f92bd06419ee0bcfad97f706f7bf607f522e
danujav <danujagreru@gmail.com>
2024-05-06 13:49:04 +0530

    primitive types done

---
+++ 03PrimitiveTypes.ts
@@ -0,0 +1,26 @@
+//  TypeScript has 3 primitive types:
+    /*
+        number
+        string
+        boolean
+    */
+
+// -------------------------
+let msg: string;
+msg = "hello";
+
+// -------------------------
+
+let num: number = 10;
+let prf: number = 75.5;
+
+// -------------------------
+
+let isAlive: boolean = true;
+
+// -------------------------
+
+//  -- any type -- //
+let x: any = 10;
+x = 'hello';
+x = true;
