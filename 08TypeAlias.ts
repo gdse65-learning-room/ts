300bd68ce061b225cd841e99c2c0caacc4c198c9
danujav <danujagreru@gmail.com>
2024-05-13 10:25:15 +0530

    type aliases

---
+++ 08TypeAlias.ts
@@ -0,0 +1,57 @@
+// without using type alias
+
+let box = {
+    height: 10,
+    width: 20,
+    length: 30
+}
+
+function findBoxArea(box: {height: number, width: number, length: number}) {
+    // find area logic should be here
+}
+
+function findBoxVolume(box: {height: number, width: number, length: number}) {
+    // find volume logic should be here
+}
+
+findBoxArea(box);
+findBoxVolume(box);
+
+// -------------------------
+
+
+// create an alias called Box
+type Box = {
+    height: number;
+    width: number;
+    length: number;
+}
+
+let box2 = {
+    height: 10,
+    width: 20,
+    length: 30
+}
+
+function findBoxArea2(box: Box) {
+    // find area logic should be here
+}
+
+function findBoxVolume2(box: Box) {
+    // find volume logic should be here
+}
+
+findBoxArea2(box2);
+findBoxVolume2(box2);
+
+
+// --------------------------------
+
+type ID = number | string;
+function print(id: ID) {
+    //
+}
+
+print(10);
+print('10');
+// print(true);    //Illegal
